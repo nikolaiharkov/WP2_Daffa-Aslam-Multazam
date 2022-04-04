d3a298a970005fa9d9f6d9d28bb9cc9e65aa4a81
Nikolai Harkov <daffaaslam2001@gmail.com>
2022-04-04 14:03:06 +0700

    pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,30 @@
+<head>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url()
+?>assets\style.css">
+</head>
+
+<a href="<?php echo base_url().'index.php/web' ?>"></a><a href="<?php echo base_url().'index.php/web/about' ?>"></a>
+<section>
+    <h1><?php echo $judul ?></h1>
+    <img src="https://wallpapercave.com/wp/wp8255866.jpg" alt="flowers" class="avatar">
+    <h4>Nama</h4>
+    <ul type="disc">
+    <li>Nama Depan : Daffa</li>
+    <li>Nama Belakang : Aslam</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+    <li>Ciputat, Tangsel</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Tangerang</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+    <li>Main Valorant</li>
+    <li>Main PB</li>
+    </ul>
+</section>
